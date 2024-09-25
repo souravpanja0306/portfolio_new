e520dc830ea31a8992d0936b85450d81485c765a
souravpanja <panjasourav03@gmail.com>
2024-09-26 01:06:52 +0530

    bug fixed in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,17 +12,20 @@ const Home = () => {
 
     return (
         <>
-            <div className="flex-cols md:lg:xl:flex p-8 min-h-screen" 
-            style={{ backgroundImage: `url("/assets/sourav33.jpg")`}}>
+            <div className="flex-cols md:lg:xl:flex p-8 min-h-screen"
+                style={{ backgroundImage: `url("./assets/sourav33.jpg")` }}>
 
                 <div className='w-full md:lg:xl:w-1/3 flex justify-center md:lg:xl:p-12 '>
                     {/* <img src='/assets/sourav33.jpg' alt='Images' className='h-60 md:lg:xl:h-96 rounded-full border-4 border-solid border-slate-400 shadow-xl' /> */}
                 </div>
                 <div className='w-full md:lg:xl:w-2/3 justify-center md:lg:xl:p-12 py-2'>
                     <div className='text-white text-center md:lg:xl:text-left'>
-                        <h1 className='text-orange-500 font-bold text-4xl md:lg:xl:text-6xl pt-4'>Hi, there!</h1>
-                        <h1 className='text-green-500 font-bold text-3xl md:lg:xl:text-5xl pt-4'>This is <span className='text-5xl md:lg:xl:text-7xl text-orange-500'>Sourav Panja </span> here.....</h1>
-                        <h1 className='text-slate-400 font-bold text-2xl pt-4'>MERN Stack Developer - MongoDB || ExpressJs || ReactJs || NodeJS </h1>
+                        <h1 className='text-orange-500 font-bold text-4xl md:lg:xl:text-6xl pt-4'>Hello! It's Me</h1>
+                        <h1 className='text-green-500 font-bold text-3xl md:lg:xl:text-5xl pt-4'>
+                            <span className='text-5xl md:lg:xl:text-7xl text-orange-500'>Sourav Panja </span>
+                        </h1>
+                        <h1 className='text-slate-400 font-bold text-2xl pt-4'>And I am MERN Stack Developer.</h1>
+                        <h1 className='text-slate-400 font-bold text-2xl pt-4'>MongoDB || ExpressJs || ReactJs || NodeJS </h1>
                     </div>
                     <div className='flex text-center justify-center md:lg:xl:p-6 py-2'>
                         <a href="https://www.instagram.com/panja0306/" target="blank">
