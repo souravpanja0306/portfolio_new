76def7c4abf5a0e8894ca74b31c21bfe3ae388dd
SOURAV <panjasourav03@gmail.com>
2024-09-27 16:11:12 +0530

    Bug Fixed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,12 +6,10 @@ const Layout = ({ children }) => {
   const themeColor = useSelector((state) => state.theme.color);
 
   return (
-    <>
-      <div id="layout" className={`font-mono min-h-screen ${themeColor == "light" ? "bg-green-50" : "bg-slate-800"}`}>
-        <Navbar />
-        {children}
-      </div>
-    </>
+    <div id="layout" className={`font-mono min-h-screen ${themeColor == "light" ? "bg-green-50" : "bg-slate-800"}`}>
+      <Navbar />
+      {children}
+    </div>
   )
 }
 
